328e3770f4cd84a1fc0669b8fb95a0d8bad5fd04
joepamedia <josephpalmisano18@gmail.com>
2022-04-21 17:50:46 -0500

    folder structure & files started

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const userSchema = require("./User");
+
+// Schema to create Student model
+const studentSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model("student", studentSchema);
+
+module.exports = Student;
